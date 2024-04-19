c50b3f600dc43b6cf76f547e552edaa7420d1fcc
fabian-stettler <fabian.stettler@hispeed.ch>
2024-04-19 18:00:10 +0200

    Uebungen 9 AD

--- week9/Sort.java
+++ week9/Sort.java
@@ -3,7 +3,7 @@ package Uebungen_AD.week9;
 public class Sort {
 
 
-    public void insertionSort2(int array[]){
+    public void insertionSort(int array[]){
         int currentElement;
         int j;
         for (int i = 1; i < array.length; i++){
@@ -15,29 +15,77 @@ public class Sort {
             }
             //set element
             array[j] = currentElement;
+        }
+    }
+
+    public void insertionSort2WithBinarySearch(int array[]){
+        int currentElement;
+        int j;
+        for (int i = 1; i < array.length; i++){
+            currentElement = array[i];
+            j = i;
+            int insertPosition = binarySearch(array, i);
+            //shift elements
+            while(j > insertPosition){
+                array[j] = array[j-1];
+                j--;
+            }
+            //set element
+            array[j] = currentElement;
 
         }
     }
 
-
-
-
-    public static void main(String[] args){
-        int[] arrayTest = new int[10];
-        int count = 10;
-        for (int i = 0; i < 10; i++){
-            arrayTest[i] = count;
-            count--;
+    public void selectionSort(int array[]){
+        for (int j = 0; j < array.length; j++) {
+            int currentMinIndex = j;
+            for (int i = j; i < array.length; i++) {
+                if (array[currentMinIndex] > array[i]) {
+                    currentMinIndex = i;
+                }
+            }
+            //swap elements
+            int a = array[j];
+            array[j] = array[currentMinIndex];
+            array[currentMinIndex] = a;
         }
-        for (int i = 0; i < arrayTest.length; i++){
-            System.out.print(arrayTest[i] + " ");
-        }
-        Sort sortingObject = new Sort();
-        sortingObject.insertionSort2(arrayTest);
+    }
 
-        for (int i = 0; i < arrayTest.length; i++){
-            System.out.print(arrayTest[i] + " ");
+    public void bubbleSort(int array[]){
+        int boundary = array.length -1;
+        while (boundary > 0){
+            for (int i = 0; i < boundary; i++) {
+                //swap
+                if (array[i] > array[i + 1]){
+                    int temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+                }
+            }
+            boundary--;
         }
+    }
+
+    public void bubbleSort2(int array[]){
+        int boundary = array.length -1;
+        boolean abortBubbleSort;
+        while (boundary > 0){
+            abortBubbleSort = true;
+            for (int i = 0; i < boundary; i++) {
+                //swap
+                if (array[i] > array[i + 1]){
+                    int temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+                    abortBubbleSort = false;
+                }
+            }
+            boundary--;
+            if (abortBubbleSort){
+                return;
+            }
+        }
+        return;
     }
 
     /**
@@ -59,24 +107,33 @@ public class Sort {
             if (array[mid] == searchedElement){
                 //we want the new place to insert an element, so we increase the position with + 1
                 return mid + 1;
-
             }
-
             else if (array[mid] < searchedElement){
                 lower = mid + 1;
-                returnUpper = false;
-
             }
             else {
                 upper = mid -1;
-                returnUpper = true;
             }
         }
-        if (returnUpper){
-            return upper;
+        return lower;
+    }
+
+    public static void main(String[] args){
+        int[] arrayTest = new int[10];
+        int count = 10;
+        for (int i = 0; i < 10; i++){
+            arrayTest[i] = count;
+            count--;
         }
-        else {
-            return lower;
+        for (int i = 0; i < arrayTest.length; i++){
+            System.out.print(arrayTest[i] + " ");
+        }
+        Sort sortingObject = new Sort();
+        sortingObject.insertionSort(arrayTest);
+
+        for (int i = 0; i < arrayTest.length; i++){
+            System.out.print(arrayTest[i] + " ");
         }
     }
+
 }
--- week9/TimeMeasuringSortingAlgorithms.java
+++ week9/TimeMeasuringSortingAlgorithms.java
@@ -2,15 +2,16 @@ package Uebungen_AD.week9;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.function.Consumer;
+
 import org.apache.commons.lang3.ArrayUtils;
 
 
 public class TimeMeasuringSortingAlgorithms {
-    private final int LENGTH = 200_000;
+    private final int LENGTH = 1_000_000;
     int[] sortedArray = new int[LENGTH];
     int[] reversedArray = new int[LENGTH];
     int[] randomArray = new int[LENGTH];
-    private final Sort sortingObject = new Sort();
 
     private void initializeRandomArray(int[] array){
         Random rand = new Random();
@@ -25,8 +26,8 @@ public class TimeMeasuringSortingAlgorithms {
         ArrayUtils.reverse(reversedArray);
 
     }
-    public void timeMeasuringInsertionSort(){
-        int passes = 10;
+    public void timeMeasuringSortingAlgos(Consumer<int[]> sortMethod){
+        int passes = 1;
         long timeSortingReversedArray = 0L;
         long timeSortingSortedArray = 0L;
         long timeSortingRandomArray = 0L;
@@ -38,24 +39,24 @@ public class TimeMeasuringSortingAlgorithms {
 
             //measure time with sorted array
             long t1 = System.currentTimeMillis();
-            sortingObject.insertionSort2(sortedArrayCopy);
+            sortMethod.accept(sortedArrayCopy);
             long t2 = System.currentTimeMillis();
 
             //measure time with reversed array
             long t12 = System.currentTimeMillis();
-            sortingObject.insertionSort2(reversedArrayCopy);
+            sortMethod.accept(reversedArrayCopy);
             long t22 = System.currentTimeMillis();
 
             //measure time of random array
             long t13 = System.currentTimeMillis();
-            sortingObject.insertionSort2(randomArrayCopy);
+            sortMethod.accept(randomArrayCopy);
             long t23 = System.currentTimeMillis();
 
             timeSortingRandomArray += (t23 - t13);
             timeSortingSortedArray += (t2 - t1);
             timeSortingReversedArray += (t22 - t12);
 
-            if (i <= 1){
+            if (i == 1 || i == 2){
                 System.out.println("Test if two consequitive passes take equally long");
                 System.out.println("Absolute Time for Sorting Random Array at Iteration " + i +": "  + (t23 - t13));
                 System.out.println("Absolute Time for Sorting Reversed Array at Iteration " + i +": " + (t22 - t12));
@@ -73,18 +74,18 @@ public class TimeMeasuringSortingAlgorithms {
         System.out.println("\nAbsolute Time for Sorting Random Array: " +timeSortingRandomArray);
         System.out.println("Absolute Time for Sorting Reversed Array: " + timeSortingReversedArray);
         System.out.println("Absolute Time for Sorting Sorted Array: "+ timeSortingSortedArray);
-
-
-
     }
 
     public static void main(String args[]){
         TimeMeasuringSortingAlgorithms measuringObject = new TimeMeasuringSortingAlgorithms();
+        Sort sortingObject = new Sort();
+
         measuringObject.initializeRandomArray(measuringObject.sortedArray);
         measuringObject.initializeRandomArray(measuringObject.reversedArray);
         measuringObject.initializeRandomArray(measuringObject.randomArray);
         measuringObject.prepareSpecialArrays();
 
-        measuringObject.timeMeasuringInsertionSort();
+        Consumer<int[]> methodReference = sortingObject::insertionSort2WithBinarySearch;
+        measuringObject.timeMeasuringSortingAlgos(methodReference);
     }
 }
