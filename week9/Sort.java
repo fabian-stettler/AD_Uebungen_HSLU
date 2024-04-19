7b772c66975e861c87294e5836446dd575f59103
fabian-stettler <fabian.stettler@hispeed.ch>
2024-04-19 14:05:41 +0200

    Uebungen 2 AD

--- week9/Sort.java
+++ week9/Sort.java
@@ -1,2 +1,82 @@
-package Uebungen_AD.week9;public class insertionSort {
+package Uebungen_AD.week9;
+
+public class Sort {
+
+
+    public void insertionSort2(int array[]){
+        int currentElement;
+        int j;
+        for (int i = 1; i < array.length; i++){
+            currentElement = array[i];
+            j = i;
+            while (j > 0 && currentElement < array[j-1]){
+                array[j] = array[j-1];
+                j--;
+            }
+            //set element
+            array[j] = currentElement;
+
+        }
+    }
+
+
+
+
+    public static void main(String[] args){
+        int[] arrayTest = new int[10];
+        int count = 10;
+        for (int i = 0; i < 10; i++){
+            arrayTest[i] = count;
+            count--;
+        }
+        for (int i = 0; i < arrayTest.length; i++){
+            System.out.print(arrayTest[i] + " ");
+        }
+        Sort sortingObject = new Sort();
+        sortingObject.insertionSort2(arrayTest);
+
+        for (int i = 0; i < arrayTest.length; i++){
+            System.out.print(arrayTest[i] + " ");
+        }
+    }
+
+    /**
+     *
+     * @param array
+     * @param firstUpperBoundary index of the element to be sorted
+     * @return correct position in array
+     */
+    private int binarySearch(int[] array, int firstUpperBoundary){
+        int indexCurrentElement = firstUpperBoundary;
+        int searchedElement = array[firstUpperBoundary];
+        int lower = 0;
+        int upper = firstUpperBoundary-1;
+        boolean returnUpper = false;
+
+        while(lower <= upper){
+            int mid = lower + (upper-lower)/2;
+
+            if (array[mid] == searchedElement){
+                //we want the new place to insert an element, so we increase the position with + 1
+                return mid + 1;
+
+            }
+
+            else if (array[mid] < searchedElement){
+                lower = mid + 1;
+                returnUpper = false;
+
+            }
+            else {
+                upper = mid -1;
+                returnUpper = true;
+            }
+        }
+        if (returnUpper){
+            return upper;
+        }
+        else {
+            return lower;
+        }
+    }
 }
--- week9/TimeMeasuringSortingAlgorithms.java
+++ week9/TimeMeasuringSortingAlgorithms.java
@@ -1,2 +1,90 @@
-package Uebungen_AD.week9;public class TimeMeasuring {
+package Uebungen_AD.week9;
+
+import java.util.Arrays;
+import java.util.Random;
+import org.apache.commons.lang3.ArrayUtils;
+
+
+public class TimeMeasuringSortingAlgorithms {
+    private final int LENGTH = 200_000;
+    int[] sortedArray = new int[LENGTH];
+    int[] reversedArray = new int[LENGTH];
+    int[] randomArray = new int[LENGTH];
+    private final Sort sortingObject = new Sort();
+
+    private void initializeRandomArray(int[] array){
+        Random rand = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(LENGTH); // Random integer between 0 and 99
+        }
+    }
+
+    private void prepareSpecialArrays(){
+        Arrays.sort(sortedArray);
+        Arrays.sort(reversedArray);
+        ArrayUtils.reverse(reversedArray);
+
+    }
+    public void timeMeasuringInsertionSort(){
+        int passes = 10;
+        long timeSortingReversedArray = 0L;
+        long timeSortingSortedArray = 0L;
+        long timeSortingRandomArray = 0L;
+        for (int i = 0; i < passes; i++){
+            //clone arrays
+            int[] sortedArrayCopy = sortedArray.clone();
+            int[] reversedArrayCopy = reversedArray.clone();
+            int[] randomArrayCopy = randomArray.clone();
+
+            //measure time with sorted array
+            long t1 = System.currentTimeMillis();
+            sortingObject.insertionSort2(sortedArrayCopy);
+            long t2 = System.currentTimeMillis();
+
+            //measure time with reversed array
+            long t12 = System.currentTimeMillis();
+            sortingObject.insertionSort2(reversedArrayCopy);
+            long t22 = System.currentTimeMillis();
+
+            //measure time of random array
+            long t13 = System.currentTimeMillis();
+            sortingObject.insertionSort2(randomArrayCopy);
+            long t23 = System.currentTimeMillis();
+
+            timeSortingRandomArray += (t23 - t13);
+            timeSortingSortedArray += (t2 - t1);
+            timeSortingReversedArray += (t22 - t12);
+
+            if (i <= 1){
+                System.out.println("Test if two consequitive passes take equally long");
+                System.out.println("Absolute Time for Sorting Random Array at Iteration " + i +": "  + (t23 - t13));
+                System.out.println("Absolute Time for Sorting Reversed Array at Iteration " + i +": " + (t22 - t12));
+                System.out.println("Absolute Time for Sorting Sorted Array: at Iteration " + i +": "+ (t2 - t1));
+
+            }
+        }
+        System.out.println("Sorting an array with " + LENGTH + " elements and " + passes + " passes");
+        System.out.println("--------------------------");
+
+        System.out.println("Average Time for Sorting Random Array: " +timeSortingRandomArray/passes);
+        System.out.println("Average Time for Sorting Reversed Array: " + timeSortingReversedArray/passes);
+        System.out.println("Average Time for Sorting Sorted Array: "+ timeSortingSortedArray/passes);
+
+        System.out.println("\nAbsolute Time for Sorting Random Array: " +timeSortingRandomArray);
+        System.out.println("Absolute Time for Sorting Reversed Array: " + timeSortingReversedArray);
+        System.out.println("Absolute Time for Sorting Sorted Array: "+ timeSortingSortedArray);
+
+
+
+    }
+
+    public static void main(String args[]){
+        TimeMeasuringSortingAlgorithms measuringObject = new TimeMeasuringSortingAlgorithms();
+        measuringObject.initializeRandomArray(measuringObject.sortedArray);
+        measuringObject.initializeRandomArray(measuringObject.reversedArray);
+        measuringObject.initializeRandomArray(measuringObject.randomArray);
+        measuringObject.prepareSpecialArrays();
+
+        measuringObject.timeMeasuringInsertionSort();
+    }
 }
